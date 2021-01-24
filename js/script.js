1698b71769fec52de77b2ae95ac7699dab3177ce
jai-sharma <sendtojsharma@gmail.com>
2021-01-24 15:26:22 +0530

    added js logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-// type your js logic here...
+const progress = document.getElementById('progress');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    if (currentActive === 1) {
+        prevBtn.disabled = true;
+    } else if (currentActive === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
+
+    const actives = document.querySelectorAll('.active');
+    progress.style.width =
+        ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+};
+
+nextBtn.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
